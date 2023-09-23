6f97e4f73f06bd401fe286b04a87599d1debe92f
fldhhh <1369196221@qq.com>
2023-09-23 17:45:06 +0800

    更新展示新闻类别与顺序

--- src/store/index.js
+++ src/store/index.js
@@ -11,31 +11,31 @@ export const mainStore = defineStore("main", {
         {
           label: "哔哩哔哩",
           value: "bilibili",
-          order: 0,
+          order: 9,
           show: true,
         },
         {
           label: "微博",
           value: "weibo",
-          order: 1,
+          order: 0,
           show: true,
         },
         {
           label: "抖音",
           value: "douyin",
-          order: 2,
+          order: 11,
           show: true,
         },
         {
           label: "知乎",
           value: "zhihu",
-          order: 3,
+          order: 6,
           show: true,
         },
         {
           label: "36氪",
           value: "36kr",
-          order: 4,
+          order: 15,
           show: true,
         },
         {
@@ -47,49 +47,97 @@ export const mainStore = defineStore("main", {
         {
           label: "少数派",
           value: "sspai",
-          order: 6,
+          order: 16,
           show: true,
         },
         {
           label: "IT之家",
           value: "ithome",
-          order: 7,
+          order: 18,
           show: true,
         },
         {
           label: "澎湃新闻",
           value: "thepaper",
-          order: 8,
+          order: 4,
           show: true,
         },
         {
           label: "今日头条",
           value: "toutiao",
-          order: 9,
+          order: 1,
           show: true,
         },
         {
           label: "百度贴吧",
           value: "tieba",
-          order: 10,
+          order: 8,
           show: true,
         },
         {
           label: "稀土掘金",
           value: "juejin",
-          order: 11,
+          order: 17,
           show: true,
         },
         {
           label: "腾讯新闻",
           value: "newsqq",
-          order: 12,
+          order: 2,
           show: true,
         },
         {
           label: "豆瓣新片榜",
           value: "douban_new",
+          order: 7,
+          show: true
+        },
+        {
+          label: "历史上的今天",
+          value: "calendar",
+          order: 19,
+          show: true
+        },
+        {
+          label: "抖音热点榜",
+          value: "douyin_new",
+          order: 12,
+          show: true
+        },
+        {
+          label: "抖音热歌榜",
+          value: "douyin_music",
           order: 13,
+          show: true
+        },
+        {
+          label: "网易新闻",
+          value: "netease",
+          order: 3,
+          show: true
+        },
+        {
+          label: "英雄联盟",
+          value: "lol",
+          order: 20,
+          show: true
+        },
+        {
+          label: "原神",
+          value: "genshin",
+          order: 21,
+          show: true
+        },
+        {
+          label: "微信读书",
+          value: "genshin",
+          order: 10,
+          show: true
+        },
+        {
+          label: "快手",
+          value: "kuaishou",
+          order: 14,
           show: true
         }
       ],
