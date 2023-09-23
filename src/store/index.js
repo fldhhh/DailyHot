4cd1bf8a02d716d0748311db8dd068fe01d60cd0
fldhhh <1369196221@qq.com>
2023-09-23 18:34:18 +0800

    提交新增图标

--- src/store/index.js
+++ src/store/index.js
@@ -9,82 +9,46 @@ export const mainStore = defineStore("main", {
       // 新闻类别
       newsArr: [
         {
-          label: "哔哩哔哩",
-          value: "bilibili",
-          order: 9,
-          show: true,
-        },
-        {
           label: "微博",
           value: "weibo",
           order: 0,
-          show: true,
-        },
-        {
-          label: "抖音",
-          value: "douyin",
-          order: 11,
-          show: true,
-        },
-        {
-          label: "知乎",
-          value: "zhihu",
-          order: 6,
-          show: true,
-        },
-        {
-          label: "36氪",
-          value: "36kr",
-          order: 15,
-          show: true,
-        },
-        {
-          label: "百度",
-          value: "baidu",
-          order: 5,
-          show: true,
-        },
-        {
-          label: "少数派",
-          value: "sspai",
-          order: 16,
-          show: true,
-        },
-        {
-          label: "IT之家",
-          value: "ithome",
-          order: 18,
-          show: true,
-        },
-        {
-          label: "澎湃新闻",
-          value: "thepaper",
-          order: 4,
-          show: true,
+          show: true
         },
         {
           label: "今日头条",
           value: "toutiao",
           order: 1,
-          show: true,
-        },
-        {
-          label: "百度贴吧",
-          value: "tieba",
-          order: 8,
-          show: true,
-        },
-        {
-          label: "稀土掘金",
-          value: "juejin",
-          order: 17,
-          show: true,
+          show: true
         },
         {
           label: "腾讯新闻",
           value: "newsqq",
           order: 2,
-          show: true,
+          show: true
+        },
+        {
+          label: "网易新闻",
+          value: "netease",
+          order: 3,
+          show: true
+        },
+        {
+          label: "澎湃新闻",
+          value: "thepaper",
+          order: 4,
+          show: true
+        },
+        {
+          label: "百度",
+          value: "baidu",
+          order: 5,
+          show: true
+        },
+        {
+          label: "知乎",
+          value: "zhihu",
+          order: 6,
+          show: true
         },
         {
           label: "豆瓣新片榜",
@@ -93,10 +57,28 @@ export const mainStore = defineStore("main", {
           show: true
         },
         {
-          label: "历史上的今天",
-          value: "calendar",
-          order: 19,
+          label: "百度贴吧",
+          value: "tieba",
+          order: 8,
+          show: true,
+        },
+        {
+          label: "哔哩哔哩",
+          value: "bilibili",
+          order: 9,
           show: true
+        },
+        {
+          label: "微信读书",
+          value: "weread",
+          order: 10,
+          show: true
+        },
+        {
+          label: "抖音",
+          value: "douyin",
+          order: 11,
+          show: true,
         },
         {
           label: "抖音热点榜",
@@ -111,9 +93,39 @@ export const mainStore = defineStore("main", {
           show: true
         },
         {
-          label: "网易新闻",
-          value: "netease",
-          order: 3,
+          label: "快手",
+          value: "kuaishou",
+          order: 14,
+          show: true
+        },
+        {
+          label: "36氪",
+          value: "36kr",
+          order: 15,
+          show: true
+        },
+        {
+          label: "少数派",
+          value: "sspai",
+          order: 16,
+          show: true
+        },
+        {
+          label: "稀土掘金",
+          value: "juejin",
+          order: 17,
+          show: true
+        },
+        {
+          label: "IT之家",
+          value: "ithome",
+          order: 18,
+          show: true
+        },
+        {
+          label: "历史上的今天",
+          value: "calendar",
+          order: 19,
           show: true
         },
         {
@@ -127,20 +139,8 @@ export const mainStore = defineStore("main", {
           value: "genshin",
           order: 21,
           show: true
-        },
-        {
-          label: "微信读书",
-          value: "genshin",
-          order: 10,
-          show: true
-        },
-        {
-          label: "快手",
-          value: "kuaishou",
-          order: 14,
-          show: true
         }
-      ],
+      ],      
       // 链接跳转方式
       linkOpenType: "open",
       // 页头固定
